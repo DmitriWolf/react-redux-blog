2cf0a68443d8b43031abb843a8998788314b7c1d
Dmitri Wolf <Dmitri.a.wolf@gmail.com>
2018-01-24 22:12:55 -0600

    store and sync with browser cookie as well as redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,22 @@
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
+var cookies = new Cookies();
+
 export const addQuote = quote => {
   return {
     type: 'ADD_QUOTE',
     quote,
   };
 }
+
 export const addContent = content => {
+	const cookieOptions = {
+		path: '/',
+		maxAge: 5256000, // 2 months
+	};
+
+	cookies.set('content', content.content, cookieOptions);
+
   return {
     type: 'ADD_CONTENT',
     content,
