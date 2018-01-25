8e36a53bee2816bdc268137b39f7320f77e5fddb
Dmitri Wolf <Dmitri.a.wolf@gmail.com>
2018-01-24 22:12:09 -0600

    rename to content

--- src/containers/RichEditorContainer.js
+++ src/containers/RichEditorContainer.js
@@ -10,8 +10,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    addContent: quote => {
-      dispatch(addContent(quote))
+    addContent: content => {
+      dispatch(addContent(content));
     }
   }
 }
