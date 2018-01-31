0343cc86d34113dda34af841ecce37275c45641e
Dmitri Wolf <Dmitri.a.wolf@gmail.com>
2018-01-30 21:24:31 -0600

    save updates redux state

--- src/reducers/content.js
+++ src/reducers/content.js
@@ -1,14 +1,16 @@
 const content = (state = {}, action) => {
+  const newState = Object.assign({}, state);
+
   switch (action.type) {
   	case 'LOAD_INITIAL_CONTENT': 
   		return action.pages;
 
     case 'SAVE_CONTENT':
-			console.log('SAVE_CONTENT - action: ', action);
-			return state;
+      const pageId = Object.keys(action.page)[0];
+      newState[pageId] = action.page[pageId];
+      return newState;
 
     case 'ADD_NEW_PAGE':
-			const newState = Object.assign({}, state);
 			const newPageId = Object.keys(action.pageObject)[0];
 			newState[newPageId] = action.pageObject[newPageId];
 			return newState;
